bce5450bd09f931915bb7ccf76831d2e6a4235d5
BelovaMP <83486459+BelovaMP@users.noreply.github.com>
2024-01-30 14:10:01 +0300

    Добавьте файлы проекта.

---
+++ testN/Program.cs
@@ -0,0 +1,49 @@
+﻿    double weight, height;
+repidWeight:
+try
+{
+    Console.WriteLine("Введите свой вес в килограммах:");
+    weight = Convert.ToDouble(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Введите корректное число");
+    goto repidWeight;
+}
+repidHeight:
+try
+{
+    Console.WriteLine("Введите свой рост в сантиметрах:");
+    height = Convert.ToDouble(Console.ReadLine());
+}
+
+catch
+{
+    Console.WriteLine("Введите корректное число");
+    goto repidHeight;
+}
+
+
+    height *= 0.01; 
+    BodyMassIndex(weight, height);
+    Console.ReadLine();
+
+    void BodyMassIndex(double weight, double height)
+{
+    double bmi = weight / Math.Pow(height, 2);
+    bmi = Math.Truncate(bmi);
+
+    if (bmi < 18)
+    {
+        Console.WriteLine("Индекс массы тела: {0} - Ниже нормального", bmi);
+    }
+    else if (18 < bmi && bmi < 25)
+    {
+        Console.WriteLine("Индекс массы тела: {0} - Нормальный", bmi);
+    }
+    else
+    {
+        Console.WriteLine("Индекс массы тела: {0} - Выше нормального", bmi);
+    }
+}
+    
